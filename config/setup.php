4fc24956da4b41f8e717e2bd4410ee2b5ed431a3
Pierre NARDOZI <pnardozi@e2r1p4.42.fr>
2018-04-19 17:00:01 +0200

    ok

--- config/setup.php
+++ config/setup.php
@@ -1,12 +1,20 @@
 #!/usr/bin/php
 <?php
-include 'database.php'
+include 'database.php';
 
 try {
 	$dbh = new PDO($DB_DNS_EMPTY, $DB_USER, $DB_PASSWD);
-	$req = "CREATE DATABASE IF NOT EXISTS '".$DB_NAME."' CHARACTER SET 'utf8'";
+	$req = "CREATE DATABASE IF NOT EXISTS ".$DB_NAME." CHARACTER SET utf8";
+	echo $req;
 	$dbh->prepare($req)->execute();
+	echo "DB create sucess\n";
 } catch (PDOException $e) {
-	echo 'Error connexion : ' $e->getMessage();
+	echo 'Error connexion : '.$e->getMessage();
+	exit(-1);
 }
+try {
+	$dbh = new PDO($DB_DNS, $DB_USER, $DB_PASSWD);
+	echo "Connec
+
+
 ?>
