c83842df079eae00e904b4a33f5eb504304eef32
Pierre NARDOZI <pnardozi@e2r2p3.42.fr>
2018-04-30 17:35:25 +0200

    en modif

--- models/model_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-function get_db() {
-	require './config/database.php';
-	$dbh = new PDO($DB_DNS, $DB_USER, $DB_PASSWD);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return ($dbh);
-}
-?>
