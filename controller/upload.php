2dd2c31ca42569b62133154f51a612b11365ccb7
Pierre NARDOZI <pnardozi@e2r3p2.42.fr>
2018-06-14 17:26:12 +0200

    ok

--- controller/upload.php
+++ controller/upload.php
@@ -10,12 +10,30 @@ function uploadImg() {
 		$img = $_POST['img'];
 		$id = $_SESSION['id'];
 
+		$img = str_replace('data:image/png;base64,', '', $img);
+		$img = str_replace(' ', '+', $img);
+		$data = base64_decode($img);
+
 		$uiid = uniqid();
 
 		if (!file_exists($montageDir)) {
 			mkdir($montageDir);
 		}
-		file_put_contents($montageDir . $uiid . '.jpg', $img);
+		file_put_contents($montageDir . $uiid . '.png', $data);
+
+		// On charge d'abord les images
+		$source = imagecreatefrompng("./font/images.png");
+		$destination = imagecreatefrompng($montageDir.$uiid.'.png');
+
+		// Les fonctions imagesx et imagesy renvoient la largeur et la hauteur d'une image
+		$largeur_source = 510;
+		$hauteur_source = 510;
+		
+		// On met le logo (source) dans l'image de destination (la photo)
+		imagecopymerge($destination, $source, 0, 0, 0, 0, $largeur_source, $hauteur_source, 60);
+
+		// On affiche l'image de destination qui a été fusionnée avec le logo
+		imagejpeg($destination, "./montage/test.png");
 
 
 
