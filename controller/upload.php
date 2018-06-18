f840c3a6ebf3a49c363f10e923e29c67aabd93dd
Pierre Nardozi <pierrenardozi@MacBook-Pro-de-Pierre.local>
2018-06-18 09:51:07 +0200

    ok

--- controller/upload.php
+++ controller/upload.php
@@ -4,23 +4,42 @@ require('./models/model_uploadImg.php');
 
 function uploadImg() {
 
+	header ("Content-type: image/png");
+
+
 	$montageDir = "./montage/";
 
 	if (isset($_POST['img']) & isset($_SESSION['id'])) {
 		$img = $_POST['img'];
+		$filter = "images"; // modifier !!
 		$id = $_SESSION['id'];
 
 		$img = str_replace('data:image/png;base64,', '', $img);
 		$img = str_replace(' ', '+', $img);
-		$data = base64_decode($img);
+		$img = base64_decode($img);
 
 		$uiid = uniqid();
 
 		if (!file_exists($montageDir)) {
 			mkdir($montageDir);
 		}
-		file_put_contents($montageDir . $uiid . '.png', $data);
+		file_put_contents($montageDir . $uiid . '.png', $img);
+		
+		$filter = imagecreatefrompng("./font/".$filter.".png");
+		$img = imagecreatefrompng("./montage/".$uiid.".png");
 
+		//resize filtre
+		$filter = resizeImg($filter);
+
+		//fusion
+		$img = fusionImg($img, $filter);
+		file_put_contents($montageDir . $uiid . '.png', $img);
+
+
+
+
+
+/*
 		// On charge d'abord les images
 		$source = imagecreatefrompng("./font/images.png");
 		$destination = imagecreatefrompng($montageDir.$uiid.'.png');
@@ -28,13 +47,13 @@ function uploadImg() {
 		// Les fonctions imagesx et imagesy renvoient la largeur et la hauteur d'une image
 		$largeur_source = 510;
 		$hauteur_source = 510;
-		
+
 		// On met le logo (source) dans l'image de destination (la photo)
 		imagecopymerge($destination, $source, 0, 0, 0, 0, $largeur_source, $hauteur_source, 60);
 
 		// On affiche l'image de destination qui a été fusionnée avec le logo
 		imagejpeg($destination, "./montage/test.png");
-
+ */
 
 
 
--- models/model_uploadImg.php
+++ models/model_uploadImg.php
@@ -15,4 +15,34 @@ function add_montage($userId, $imgPath) {
 	return (0);
 }
 
+function resizeImg($image) {
+	$source = $image;
+	$widthSource = imagesx($source);
+	$heightSource = imagesy($source);
+
+	if ($heightSource > 510 || $widthSource > 510)
+	{
+		$imageResize = imagecreatetruecolor(510, 510);
+		$widthResize = imagesx($imageResize);
+		$heightResize = imagesy($imageResize);
+		imagecopyresampled($imageResize, $source, 0, 0, 0, 0, $widthResize, $heightResize, $widthSource, $heightSource);
+		return ($imageResize);
+	} else {
+		return ($source);
+	}
+}
+
+function fusionImg($image, $filter) {
+	$destination = $image;
+	$widthDestination = imagesx($destination);
+	$heightDestination = imagesy($destination);
+	$widthFilter = imagesx($filter);
+	$heightFilter = imagesy($filter);
+	$x = 0;
+	$y = 0;
+
+	imagecopymerge($destination, $filter, $x, $y, 0, 0, $widthDestination, $heightDestination, 60);
+	return ($destination);
+}
+
 ?>
